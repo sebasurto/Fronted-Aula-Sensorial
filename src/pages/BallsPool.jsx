0e3e7a2e28261a106d2f61cce001d69a8994c399
Sergio Basurto <35089326+sebasurto@users.noreply.github.com>
2023-11-10 02:31:47 -0500

    Elementos vista de piscina de pelotas

--- src/pages/BallsPool.jsx
+++ src/pages/BallsPool.jsx
@@ -12,20 +12,6 @@ function BallsPool({ navigation }) {
     </ScrollView>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  image: {
-    width: 200, // Ajusta el ancho de la imagen según sea necesario
-    height: 200, // Ajusta la altura de la imagen según sea necesario
-    position: 'absolute',
-    top: '50%', // Ubica la imagen en el centro superior
-    transform: [{ translateY: -100 }], // Ajusta para centrar la imagen
-  },
- 
-});
+
 
 export default BallsPool;
