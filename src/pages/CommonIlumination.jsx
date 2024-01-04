c5c2bbb5fa9c325f605c2800404cd546b15b1432
Nicolás Plaza <74793371+niplinig@users.noreply.github.com>
2024-01-04 18:33:26 -0500

    Update CommonIlumination.jsx

--- src/pages/CommonIlumination.jsx
+++ src/pages/CommonIlumination.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
-import { View, Text, Button } from 'react-native';
-import styles from '../styles/styles';
+import React from "react";
+import { ScrollView, Image, View, StyleSheet } from "react-native";
+import SliderControl from "../components/TubeRoom/SliderControl";
+import styles from "../styles/styles";
+import TubeRoomButtons from "../components/TubeRoom/TubeRoomButtons";
 
 function CommonIlumination({ navigation }) {
   return (
-    <View>
-      <Text style={styles.title}> Iluminacion general</Text>
-    </View>
+    <ScrollView style={styles.mainView}>
+      <SliderControl />
+      <View>
+        <Image
+          source={require("../images/room.png")}
+          style={styles.image}
+          resizeMode="cover"
+        />
+      </View>
+      <TubeRoomButtons />
+    </ScrollView>
   );
 }
+
+const styles_2 = StyleSheet.create({
+  image: {
+    width: 200,
+    height: 200,
+  },
+});
 
 export default CommonIlumination;
